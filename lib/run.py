002123f6e0731a1c75f8f5703e13c21a16dbf9b6
tuyenubuntu <tuyen.tuyentruongthanh@gmail.com>
2025-05-31 13:57:55 +0700

    set output memory for reset all output

--- lib/run.py
+++ lib/run.py
@@ -10,9 +10,11 @@ if handle:
     channel = 16
     
     # pci.set_output_off(handle, channel)
-    pci.set_digital_output_memory(handle, enable=True)
-    pci.set_all_outputs_off(handle)
-    input_val = pci.get_16_outputs_status(handle)
-    channels = channel
-    time.sleep(2)
-    print(f" status: {input_val}")
+    # pci.set_digital_output_memory(handle, enable=True)
+    # pci.set_all_outputs_off(handle)
+    # input_val = pci.get_16_outputs_status(handle)
+    # channels = channel
+    # time.sleep(2)
+    # print(f" status: {input_val}")
+    status = pci.get_16_outputs_status(handle)
+    print (status)
